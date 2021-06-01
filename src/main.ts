f924dc4523f5cb6c4f21d0de1105d1f70a3d0667
Martijn Faber <martijn@fabtronics.nl>
2021-06-01 18:03:23 +0200

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from '@modules/app/app.module';
 import { ResponseInterceptor } from '@filters/response.interceptor';
 
 import config from '@config/config';
+import { Logger } from '@utils/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('server');
+  logger.log('starting server');
+
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
 
   app.enableCors();
   app.useGlobalInterceptors(new ResponseInterceptor());
