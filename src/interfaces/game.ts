ef0cd293b4cdc6bc8fe2778e67471fa41b72740f
Martijn Faber <martijn@fabtronics.nl>
2021-06-01 19:37:45 +0200

    fix(interfaces): made the keys optional

--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -1,7 +1,7 @@
 export default interface Game {
-  id: string;
-  ip: string;
-  code: number;
-  time_remaining: number;
-  running: true;
+  id?: string;
+  ip?: string;
+  code?: number;
+  time_remaining?: number;
+  running?: true;
 }
