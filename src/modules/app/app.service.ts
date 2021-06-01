2be7e02beef176333c0145d432823b8b0e41798a
Martijn Faber <martijn@fabtronics.nl>
2021-06-01 07:38:59 +0200

    feat(app): return author

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import config from '@config/config';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInfo() {
+    return {
+      author: config.author,
+    };
   }
 }
