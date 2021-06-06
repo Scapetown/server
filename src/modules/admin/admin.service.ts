ac8bf969c6a652a272bf2131ff970d45218516ae
Martijn Faber <martijn@fabtronics.nl>
2021-06-06 20:11:27 +0200

    refactor(countdown): refactor start function and added stop function so it stops if a new game is created

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -10,6 +10,8 @@ import { WebsocketGateway } from '@modules/websocket/websocket.gateway';
 
 @Injectable()
 export class AdminService extends Logger {
+  private game: Game;
+
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private ardruinoService: ArdruinoService,
@@ -18,21 +20,24 @@ export class AdminService extends Logger {
     super();
   }
 
-  newGame() {
-    this.cacheManager.del('game');
+  async newGame() {
+    if (this.game) {
+      this.game.time_remaining.stop();
+      this.cacheManager.del('game');
+    }
 
-    const game: Game = {
+    this.game = {
       id: uuidv4(),
       ip: generateIp(),
       code: Math.floor(1000 + Math.random() * 9000),
       time_remaining: new CountDown(config.game.duration, (duration: number) => {
-        this.websocketGateway.sendRemaining(duration);
+        this.websocketGateway.sendEvent(duration);
       }),
       running: true,
     };
 
-    this.cacheManager.set('game', game);
-    this.ardruinoService.showOnLcd(`[het IP adres is]${game.ip}`); //[ represents the first line, ] represents the secund line
+    this.cacheManager.set('game', this.game);
+    this.ardruinoService.showOnLcd(`[het IP adres is]${this.game.ip}`); //[ represents the first line, ] represents the secund line
 
     super.log('game created');
 
--- src/modules/countdown/countdown.ts
+++ src/modules/countdown/countdown.ts
@@ -1,12 +1,25 @@
 export class CountDown {
-  constructor(durationInSeconds: number, onTimerTick) {
-    const intervalId = setInterval(() => {
-      durationInSeconds -= 1;
-      if (durationInSeconds === 0) {
-        clearInterval(intervalId);
+  private durationInSeconds: number;
+  private interval;
+
+  constructor(durationInSeconds, onTimerTick) {
+    this.start(durationInSeconds, onTimerTick);
+  }
+
+  start(durationInSeconds: number, onTimerTick) {
+    this.durationInSeconds = durationInSeconds;
+
+    this.interval = setInterval(() => {
+      this.durationInSeconds -= 1;
+      if (this.durationInSeconds === 0) {
+        clearInterval(this.interval);
       }
 
-      onTimerTick(durationInSeconds);
+      onTimerTick(this.durationInSeconds);
     }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
   }
 }
