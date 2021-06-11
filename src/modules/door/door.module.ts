f5aa76c30b5a092a8ab3d3723d164dc56bef473c
Martijn Faber <martijn@fabtronics.nl>
2021-06-11 15:38:51 +0200

    feat(door): change rest to new keypad service

--- src/modules/door/door.module.ts
+++ src/modules/door/door.module.ts
@@ -1,14 +1,16 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { DoorService } from './door.service';
-import { DoorController } from './door.controller';
+// import { DoorController } from './door.controller';
+import { KeypadModule } from '@modules/keypad/keypad.module';
 
 @Module({
   imports: [
     CacheModule.register({
       ttl: null,
     }),
+    KeypadModule,
   ],
   providers: [DoorService],
-  controllers: [DoorController],
+  // controllers: [DoorController],
 })
 export class DoorModule {}
--- src/modules/door/door.service.ts
+++ src/modules/door/door.service.ts
@@ -1,35 +1,40 @@
-import {
-  Injectable,
-  CACHE_MANAGER,
-  Inject,
-  Logger,
-  HttpException,
-} from '@nestjs/common';
+import { Injectable, CACHE_MANAGER, Inject, Logger } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import Game from '@interfaces/game';
+import { KeypadService } from '@modules/keypad/keypad.service';
 
 @Injectable()
 export class DoorService extends Logger {
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
+  private doorCode;
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache, private keypadService: KeypadService) {
     super();
+    this.doorCode = 0;
+
+    this.keypadService.registerRead((code: string) => {
+      this.checkCode(code);
+    });
   }
 
-  async checkCode(body) {
-    const { code } = body;
+  async checkCode(code: string) {
     const game: Game = await this.cacheManager.get('game');
+    const ch = code[0];
 
-    if (game) {
-      if (game.code == code) {
-        return {
-          message: 'ok',
-        };
-      }
-
-      return {
-        message: 'code incorrect',
-      };
+    if (!game) {
+      console.log("currently there's no game running");
+      this.doorCode = 0;
+      return;
     }
 
-    throw new HttpException('there is currently no game running', 404);
+    if (ch >= '0' && ch <= '9') {
+      this.doorCode = this.doorCode * 10 + parseInt(ch);
+    }
+
+    if (ch === '#') {
+      if (game.code === this.doorCode) {
+        //open door servo
+      }
+      this.doorCode = 0;
+    }
   }
 }
