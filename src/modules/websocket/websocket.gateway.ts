3aa5310408a5b42f2a519ba09f29f144b3a48892
Martijn Faber <martijn@fabtronics.nl>
2021-06-11 15:39:36 +0200

    fix(websocket): remove broadcast function and use emit instead

--- src/modules/websocket/websocket.gateway.ts
+++ src/modules/websocket/websocket.gateway.ts
@@ -16,33 +16,20 @@ export class WebsocketGateway extends Logger implements OnGatewayInit, OnGateway
 
   @WebSocketServer()
   private wss: Server;
-  clients = [];
 
   public afterInit() {
     super.log('wss initialized');
   }
 
   public handleConnection(client: any) {
-    this.clients.push(client);
+    console.log('new client connected');
   }
 
   public handleDisconnect(client: any) {
-    for (let i = 0; i < this.clients.length; i++) {
-      if (this.clients[i].id === client.id) {
-        this.clients.splice(i, 1);
-        return;
-      }
-    }
-  }
-
-  private broadcast(event: unknown, message: any) {
-    const broadCastMessage = JSON.stringify(message);
-    for (const c of this.clients) {
-      c.emit(event, broadCastMessage);
-    }
+    console.log('client disconnected');
   }
 
   sendEvent(durationInSeconds: number) {
-    this.broadcast('remaining', durationInSeconds);
+    this.wss.emit('remaining', { data: durationInSeconds });
   }
 }
