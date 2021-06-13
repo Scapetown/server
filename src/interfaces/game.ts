ee013b22bbf4e7055f721d0f0be8d0bd83363fae
Martijn Faber <martijn@fabtronics.nl>
2021-06-13 14:14:59 +0200

    feat(game): add team types

--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -1,7 +1,12 @@
-export default interface Game {
+export interface Game {
   id?: string;
   ip?: string;
   code?: number;
+  team: Team;
   time_remaining?: any;
   running?: true;
 }
+
+export interface Team {
+  name: string;
+} 
